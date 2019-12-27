c56ad752702e30b23ac8c64271d84d0462b13ddd
William McCall <wnm.code@gmail.com>
2019-12-27 13:35:09 -0500

    Add view in wxpython

--- ThreadHunt.py
+++ ThreadHunt.py
@@ -1,46 +1,66 @@
-import tkinter as tk
-from PIL import Image, ImageTk
+import wx
+# from PIL import Image, ImageTk
 
 width = 1600
 height = 960
 root_coord = 0
 tile_size = 160
 
-class InanimateScreen(tk.Frame):
-    def __init__(self, master=None):    
-        tk.Frame.__init__(self, master)
-        self.master = master
-        self.pack(fill="both", expand=True)    
 
-        sky_render = ImageTk.PhotoImage(Image.open("sky160.png"))
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title='Thread Hunt', style=wx.DEFAULT_FRAME_STYLE &
+                         ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        self.Show()
+
+
+class WelcomeSplash(wx.Frame):
+    def __init__(self, parent_frame):
+        super().__init__(parent=None, title='', style=wx.DEFAULT_FRAME_STYLE &
+                         ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+
+        png = wx.Image(
+            "ThreadHunt800x320.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        wx.StaticBitmap(parent=parent_frame, id=-1, bitmap=png,
+                        pos=((width-800)/2, 160))
+
+
+class Background(wx.Frame):
+    def __init__(self, parent_frame):
+        super().__init__(parent=None, title='', style=wx.DEFAULT_FRAME_STYLE &
+                         ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
         xcoord = root_coord
         ycoord = root_coord
+
         while ycoord < height:
             while xcoord < width:
-                img = tk.Label(self, image=sky_render, borderwidth=0, highlightthickness=0)
-                img.image = sky_render
-                img.place(x=xcoord, y=ycoord)
+                png = wx.Image(
+                    "sky160.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+                wx.StaticBitmap(parent=parent_frame, id=-1, bitmap=png,
+                                pos=(xcoord, ycoord))
                 xcoord += tile_size
             ycoord += tile_size
             xcoord = root_coord
 
-        grass_render = ImageTk.PhotoImage(Image.open("grass160.png"))
+
+class Foreground(wx.Frame):
+    def __init__(self, parent_frame):
+        super().__init__(parent=None, title='', style=wx.DEFAULT_FRAME_STYLE &
+                         ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
         xcoord = root_coord
         while xcoord < width:
-            img = tk.Label(self, image=grass_render, borderwidth=0, highlightthickness=0)
-            img.image = grass_render
-            img.place(x=xcoord, y=height-tile_size)
+            png = wx.Image(
+                "grass160.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+            wx.StaticBitmap(parent=parent_frame, id=-1, bitmap=png,
+                            pos=(xcoord, height-tile_size-35))
             xcoord += tile_size
-        # self.label = tk.Label(self, text="Hello, World!")
-        # self.label.pack(padx=20, pady=20)
-        
+
+
 if __name__ == "__main__":
-    root = tk.Tk()
-
-    main = InanimateScreen(root)
-    # main.pack(fill="both", expand=True)
-
-    root.wm_title("Thread Hunt")
-    root.geometry(f"{width}x{height}")
-    root.resizable(False, False)
-    root.mainloop()
+    app = wx.App()
+    frame = MyFrame()
+    frame.SetDimensions(0, 0, width, height)
+    frame.AddChild(Background(frame))
+    frame.AddChild(WelcomeSplash(frame))
+    frame.AddChild(Foreground(frame))
+    app.MainLoop()
