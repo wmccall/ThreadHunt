299000d899dd28e73e6646b3913ef92e1a627e3a
William McCall <wnm.code@gmail.com>
2019-12-30 16:42:42 -0500

    Remove button borders

--- ThreadHunt.py
+++ ThreadHunt.py
@@ -46,31 +46,43 @@ class ClickableItems(wx.Frame):
         self.parent_frame = parent_frame
 
         start_png = wx.Image(
-            "Start320x120.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+            "Start320x120.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
         self.start_button = wx.BitmapButton(parent=parent_frame, id=2, bitmap=start_png,
-                                            pos=((width-320)/2, 540))
+                                            pos=((width-320)/2, 540), style=wx.NO_BORDER)
         self.start_button.Bind(wx.EVT_LEFT_DOWN, self.start_clicked)
+        self.start_button.Bind(wx.EVT_ENTER_WINDOW, self.start_hover)
 
         info_png = wx.Image(
-            "Info9.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+            "Info9.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
         self.info_button = wx.BitmapButton(parent=parent_frame, id=2, bitmap=info_png,
-                                           pos=(10, 10))
+                                           pos=(10, 10), style=wx.NO_BORDER)
         self.info_button.Bind(wx.EVT_LEFT_DOWN, self.info_clicked)
+        self.info_button.Bind(wx.EVT_ENTER_WINDOW, self.info_hover)
 
         settings_png = wx.Image(
-            "Settings9.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+            "Settings9.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
         self.settings_button = wx.BitmapButton(parent=parent_frame, id=2, bitmap=settings_png,
-                                               pos=(130, 10))
+                                               pos=(130, 10), style=wx.NO_BORDER)
         self.settings_button.Bind(wx.EVT_LEFT_DOWN, self.settings_clicked)
+        self.settings_button.Bind(wx.EVT_ENTER_WINDOW, self.settings_hover)
 
     def start_clicked(self, event):
         self.start_button.Destroy()
 
+    def start_hover(self, event):
+        self.start_button.SetWindowStyleFlag(wx.NO_BORDER)
+
     def info_clicked(self, event):
         self.info_button.Destroy()
 
+    def info_hover(self, event):
+        self.info_button.SetWindowStyleFlag(wx.NO_BORDER)
+
     def settings_clicked(self, event):
         self.settings_button.Destroy()
+
+    def settings_hover(self, event):
+        self.settings_button.SetWindowStyleFlag(wx.NO_BORDER)
 
 
 class Background(wx.Frame):
